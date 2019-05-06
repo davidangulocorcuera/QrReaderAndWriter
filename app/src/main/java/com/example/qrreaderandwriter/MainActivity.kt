f09a653c8063c6f7c732418106c30501fa0005f5
davidangulocorcuera <davidangulocorcuera@gmail.com>
2019-05-06 12:47:14 +0200

    working in livedata url from onActivityResult

--- app/src/main/java/com/example/qrreaderandwriter/MainActivity.kt
+++ app/src/main/java/com/example/qrreaderandwriter/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.qrreaderandwriter
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+
+
 class MainActivity : AppCompatActivity() {
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,4 +24,8 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        ReaderFragment().onActivityResult(requestCode,resultCode,intent)
+    }
+
 }
--- app/src/main/java/com/example/qrreaderandwriter/ReaderFragment.kt
+++ app/src/main/java/com/example/qrreaderandwriter/ReaderFragment.kt
@@ -1,11 +1,12 @@
 package com.example.qrreaderandwriter
 
 
-import android.Manifest
 import android.Manifest.permission.CAMERA
 import android.annotation.SuppressLint
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
@@ -14,49 +15,61 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.google.zxing.Result
-import kotlinx.android.synthetic.main.fragment_reader.*
-import me.dm7.barcodescanner.zxing.ZXingScannerView
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.fragment_reader.view.*
 
 
+class ReaderFragment : Fragment() {
 
-
-class ReaderFragment : Fragment(), ZXingScannerView.ResultHandler {
-    lateinit var scanner: ZXingScannerView
-
-
+    var mutableUrl = MutableLiveData<String>()
+    lateinit var observerUrl: Observer<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        scanner = ZXingScannerView(activity)
-        return scanner
-    }
+        return inflater.inflate(R.layout.fragment_reader, container, false).also { view ->
+             observerUrl =  object: Observer<String> {
+                override fun onChanged(url: String?) {
+                    view.tvBarcode.text = url
+                }
+            }
+            mutableUrl.observe(this, observerUrl)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (checkPermissions()){
-            scanner = ZXingScannerView(activity)
-            scanner.setResultHandler(this)
-            scanner.startCamera()
+            view.btnScan.setOnClickListener {
+                if (checkPermissions()) {
+                    initializeBarcodeScan()
+                } else {
+                    requestPermission()
+                }
+            }
 
         }
-        else{
-            requestPermission()
-        }
-
     }
 
 
-    fun checkPermissions(): Boolean{
+    private fun initializeBarcodeScan() {
+        val integrator = IntentIntegrator(activity)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES)
+        integrator.setPrompt("Apunta al qr")
+        integrator.setOrientationLocked(true)
+        integrator.setCameraId(0)
+        integrator.initiateScan()
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        val scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent)
+        mutableUrl.postValue(scanResult.contents.toString())
+    }
+
+
+    private fun checkPermissions(): Boolean {
         return (context?.let { ContextCompat.checkSelfPermission(it, CAMERA) }
                 == PackageManager.PERMISSION_GRANTED)
-
     }
 
-
-    fun requestPermission(){
+    private fun requestPermission() {
         this.activity?.let { ActivityCompat.requestPermissions(it, arrayOf(CAMERA), REQUEST_CAMERA) }
     }
 
@@ -66,27 +79,13 @@ class ReaderFragment : Fragment(), ZXingScannerView.ResultHandler {
         if (requestCode == 123) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-
             } else Toast.makeText(context, "sin permisos", Toast.LENGTH_SHORT).show()
         }
     }
 
-
-    override fun handleResult(result: Result?) {
-        tvBarcode.text = result.toString()
-        scanner.resumeCameraPreview(this)
-    }
-
-
-    override fun onPause() {
-        super.onPause()
-        scanner.stopCamera()
-    }
     companion object {
         const val REQUEST_CAMERA = 1
     }
-
-
 
 
 }
